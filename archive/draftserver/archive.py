ad28a9f9176bf8e94db72a74245ff8b2c9c0e6e4
krishna <krishna.chalise@ebpearls.com>
2017-04-05 13:49:31 +0545

    script to make archive committed

---
+++ archive/draftserver/archive.py
@@ -0,0 +1,25 @@
+import os,datetime
+
+def read_files():
+	file = open('lists.txt','r')
+	for i,line in enumerate(file):
+			make_tar(line[0:-1]);
+			
+def make_tar(line):
+	dirf = ("%s" %line)
+	filename = dirf + "\ " + str(datetime.date.today())
+	print "making tar of " + dirf
+	command ="cd " + dirf + " && tar -zcvf ../" + filename + ".tar.gz . && mv ../" + filename + ".tar.gz . && cd .. "
+	print(command)
+	output = os.system(command)
+	print(output)
+	permission_cmd = "chown -R ci:ci " + dirf
+	output1= os.system(command)
+	print "tarfile of " + dirf + "created"
+	
+def main():
+	read_files()
+	
+if __name__ == '__main__':
+    main()
+
--- server-connect/make-tar.php
+++ server-connect/make-tar.php
@@ -1,9 +1,11 @@
 <?php 
-	$filename = $_GET['file'];
-	//$filename="motherland";
+	//$filename = $_GET['file'];
+	echo "STARTING \n";
+	$filename="mid";
 	$command = "tar -zcvf " . $filename . ".tar.gz" . " ..\n";
 	echo "$command";
 	$cmd=shell_exec("$command");
+	echo "Tar created \n";
 	$command2="mv ../" . $filename . ".tar.gz" . " .";
 	echo "$command2";
 	$cmd2=shell_exec("$command2");
